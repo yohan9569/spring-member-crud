1a5f79e6604a40d4846c2629ac78bd802059fb19
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-02-10 11:59:11 +0900

    init

---
+++ src/main/java/com/example/spring_member_crud/SpringMemberCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_member_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMemberCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMemberCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_member_crud/SpringMemberCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_member_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMemberCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
