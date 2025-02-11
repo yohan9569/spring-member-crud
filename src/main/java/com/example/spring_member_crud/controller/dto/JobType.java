c2669c6de397f7efff6d309ecbeedea8d2581089
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-02-11 16:21:53 +0900

    실습 5-51: Enum 반환(직렬화) 시 : Enum 내 특정 필드만 출력 - @JsonValue 필드

--- src/main/java/com/example/spring_member_crud/controller/dto/JobType.java
+++ src/main/java/com/example/spring_member_crud/controller/dto/JobType.java
@@ -2,6 +2,7 @@ package com.example.spring_member_crud.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.Arrays;
 import java.util.List;
 import lombok.AccessLevel;
@@ -9,7 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
-@JsonFormat(shape = Shape.OBJECT)
+//@JsonFormat(shape = Shape.OBJECT)
 @Getter
 @AllArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -17,6 +18,7 @@ public enum JobType {
     DEVELOPER("Developer", Arrays.asList("Frontend", "Backend")),
     ENGINEER("Engineer", Arrays.asList("DevOps", "SRE"));
 
+    @JsonValue //Enum 내 특정 필드만 출력
     String name;
     List<String> titles;
 }
