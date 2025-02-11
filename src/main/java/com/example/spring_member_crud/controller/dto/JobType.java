63a9f328fb5659105e7dac18a30e2c6273acd7d9
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-02-11 17:49:46 +0900

    실습 5-54: Enum 반환(직렬화) 시 : 아무것도 설정하지 않으면 기본 Key 값 반환 (DEVELOPER)

--- src/main/java/com/example/spring_member_crud/controller/dto/JobType.java
+++ src/main/java/com/example/spring_member_crud/controller/dto/JobType.java
@@ -2,6 +2,7 @@ package com.example.spring_member_crud.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.Arrays;
 import java.util.List;
@@ -10,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
-//@JsonFormat(shape = Shape.OBJECT)
 @Getter
 @AllArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -21,8 +21,4 @@ public enum JobType {
     String name;
     List<String> titles;
 
-    @JsonValue //특정 메서드 결과를 출력
-    public String serialize() {
-        return String.format("명칭 : %s | 종류 : %s", this.name, titles.toString());
-    }
 }
