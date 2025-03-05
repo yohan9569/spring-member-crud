28cb69442605097943ed14189de3e445d0477478
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-03-05 22:19:07 +0900

    실습 5-66: @ControllerAdvice 에서도 JSON 형태로 반환하기 위해서는 @responsebody 필요

--- src/main/java/com/example/spring_member_crud/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/spring_member_crud/controller/advice/CustomExceptionHandler.java
@@ -20,12 +20,14 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @ControllerAdvice // controller 앞단에서 발생하는 에러를 캐치하기 위함.
 public class CustomExceptionHandler {
     // @ExceptionHandler 로 어떤 에러인지?를 명시하여 그에 해당하는 에러가 발생 시 바로 아래 정의된 메서드에서 처리를 한다.
     @ExceptionHandler // @ExceptionHandler(value = {UserNotFoundException.class, NullPointerException.class})
+    @ResponseBody
     public BaseResponse<Void> handle(CustomException e) {
         ExceptionType type = e.getType();
         log.atLevel(type.getLevel()).setCause(e).log(e.getMessage());
@@ -33,6 +35,7 @@ public class CustomExceptionHandler {
     }
 
     @ExceptionHandler // @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
     public BaseResponse<List<FieldErrorDto>> handle(MethodArgumentNotValidException e) {
         List<FieldErrorDto> errors = new ArrayList<>();
         StringBuilder messageBuilder = new StringBuilder();
@@ -50,6 +53,7 @@ public class CustomExceptionHandler {
     }
 
     @ExceptionHandler
+    @ResponseBody
     public BaseResponse<Void> handle(Exception e) {
         log.error(e.getMessage(), e);
         return BaseResponse.failure(ExceptionType.UNCLASSIFIED_ERROR);
